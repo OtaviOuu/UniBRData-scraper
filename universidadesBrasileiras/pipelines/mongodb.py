ca7e1d69b34d4e4474b4b72fd1a160242a109f90
OtaviOuu <conta.otaviou@gmail.com>
2024-09-26 10:19:55 -0300

    random user agents + ITA

--- universidadesBrasileiras/pipelines/mongodb.py
+++ universidadesBrasileiras/pipelines/mongodb.py
@@ -5,15 +5,13 @@ from pymongo import MongoClient
 
 class MongoDBPipeline:
     def open_spider(self, spider):
-        self.client = MongoClient(spider.settings.get('MONGODB_URI'))
-        self.db = self.client[spider.settings.get('MONGODB_DATABASE')]
-        self.collection = self.db['test']
-    
+        self.client = MongoClient(spider.settings.get("MONGODB_URI"))
+        self.db = self.client[spider.settings.get("MONGODB_DATABASE")]
+        self.collection = self.db["test"]
 
     def close_spider(self, spider):
         self.client.close()
-        
-        
+
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
         self.collection.insert_one(adapter.asdict())
---
+++ universidadesBrasileiras/spiders/undergraduate/ita.py
@@ -0,0 +1,27 @@
+from typing import Iterable
+from scrapy import Spider
+from scrapy.http import Request, Response
+
+
+from ...items import UniversidadesbrasileirasItem
+
+
+class ITA(Spider):
+    name = "ita-undergraduate"
+    base_url = "http://www.ita.br"
+
+    def start_requests(self) -> Iterable[Request]:
+        yield Request(url=f"{self.base_url}/grad", callback=self.extractPrograms)
+
+    def extractPrograms(self, response: Response):
+
+        # Removendo o "Curso Fundamental". Irrelevante por agora
+        programsList = response.css("#dhtml_menu-3277-1 li a::text").getall().slice(1)
+
+        item = UniversidadesbrasileirasItem()
+        for program in programsList:
+            item["program"] = program
+            item["city"] = "São Jose dos Campos"
+            item["state"] = "São Paulo"
+
+            yield item
